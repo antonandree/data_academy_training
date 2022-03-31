1b2b84cf4aab2957f0851f2f766563d90bb2e32b
anton.andree@solita.fi <anton.andree@solita.fi>
2022-03-31 12:59:22 +0000

    flatten live data

---
+++ models/raw/stg_citibike.sql
@@ -0,0 +1,14 @@
+select
+    f.value:num_bikes_disabled::int as num_bikes_disabled,
+    f.value:num_docks_available::int as num_docks_available,
+    f.value:station_status::string as station_status,
+    f.value:legacy_id::string as legacy_id,
+    f.value:num_ebikes_available::int as num_ebikes_available,
+    f.value:station_id::string as station_id,
+    f.value:num_bikes_available::int as num_bikes_available,
+    f.value:is_installed::int as is_installed,
+    f.value:is_returning::int as is_returning,
+    f.value:num_docks_disabled::int num_docks_disabled,
+    f.value:last_reported::int last_reported,
+    f.value:is_renting::int as is_renting
+from {{ source('citi_bike', 'landing_table') }}, table(flatten(v:data:stations)) f;
