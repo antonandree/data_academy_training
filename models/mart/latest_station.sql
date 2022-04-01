9acbfe3c556ddd131d8e8f020a49f2f8d7794ce3
anton.andree@solita.fi <anton.andree@solita.fi>
2022-04-01 07:27:32 +0000

    select latest status query

---
+++ models/mart/latest_station.sql
@@ -0,0 +1,17 @@
+SELECT st.station_id,
+    li.last_reported,
+    li.num_docks_available,
+    li.station_status,
+    li.legacy_id,
+    li.num_ebikes_available,
+    li.station_id,
+    li.num_bikes_available,
+    li.is_installed,
+    li.is_returning,
+    li.num_docks_disabled,
+    li.last_reported,
+    li.is_renting
+FROM {{ ref('citibike_static_flatten') }} AS st
+LEFT JOIN citibike_anton.raw_base.citibike_live_flatten AS li 
+ON st.station_id = li.station_id
+qualify row_number() over (partition by st.station_id ORDER BY li.last_reported DESC) = 1;
--- models/mart/latest_station_data.sql
+++
@@ -1,4 +0,0 @@
-select st.station_id, last_reported
-from {{ ref('citibike_static_flatten') }} as st
-left join citibike_anton.raw_base.citibike_live_flatten as li 
-on st.station_id = li.station_id
